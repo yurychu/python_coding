d7a0e6608f7ada2eb4462e0cf82c7901d116a60e
yurychu <tesewar@mail.ru>
2016-02-22 19:21:45 +0300

    доработка hours_ahead под шаблонную систему

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -23,5 +23,4 @@ def hours_ahead(request, offset):
     except ValueError:
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
+    return render_to_response('hours_ahead.html', {'offset': offset, 'dt': dt})
