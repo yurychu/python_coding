5c02288877b21fdfefbc16624be84773c73aa260
yurch <tesewar@mail.ru>
2016-02-25 13:58:22 +0300

    упрощение через проверку параметров

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -37,15 +37,14 @@ def display_meta(request):
     return HttpResponse('<table>%s</table>' % '\n'.join(html))
 
 
-def search_form(request):
-    return render_to_response('search_form.html')
-
-
 def search(request):
-    if 'q' in request.GET and request.GET['q'] != '':
+    error = False
+    if 'q' in request.GET:
         q = request.GET['q']
-        books = Book.objects.filter(title__icontains=q)
-        return render_to_response('search_results.html',
-                                  {'books': books, 'query': q})
-    else:
-        return HttpResponse('Please submit a search term')
+        if not q:
+            error = True
+        else:
+            books = Book.objects.filter(title__icontains=q)
+            return render_to_response('search_results.html',
+                                    {'books': books, 'query': q})
+    return render_to_response('search_form.html', {'error': error})
