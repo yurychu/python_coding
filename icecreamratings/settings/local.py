cea36910773b8a6d2749144216c8857e1a8a74b9
yurychu <tesewar@mail.ru>
2016-03-13 22:46:27 +0300

    разделение сеттингсов

---
+++ icecreamratings/settings/local.py
@@ -0,0 +1,16 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# INSTALLED_APPS +=('debug_toolbar',)
