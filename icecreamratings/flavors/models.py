4713f068bcc08a1b7be41356dce151196914895b
yurychu <tesewar@mail.ru>
2016-03-15 22:46:33 +0300

    кастомные модели менеджера объектов моделей

--- icecreamratings/flavors/models.py
+++ icecreamratings/flavors/models.py
@@ -1,7 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
 from core.models import TimeStampedModel
 
 
 class Flavor(TimeStampedModel):
     title = models.CharField(max_length=200)
+
+
+class PublishedManager(models.Manager):
+
+    use_for_related_fields = True
+
+    def published(self, **kwargs):
+        return self.filter(pub_date__lte=timezone.now(), **kwargs)
+
+
+class FlavorReview(models.Model):
+    review = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+
+    # добавляем кастомную модель менеджера (переименовываем objects)
+    objects = PublishedManager()
--- icecreamratings/settings/base.py
+++ icecreamratings/settings/base.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'flavors',
 ]
 
 MIDDLEWARE_CLASSES = [
