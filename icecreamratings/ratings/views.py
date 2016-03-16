bca0c561c69ab4048aaf92b9edc8611993cd031e
yurychu <tesewar@mail.ru>
2016-03-16 21:17:54 +0300

    обработка получения нескольких объектов, когда ожидается один

--- icecreamratings/ratings/views.py
+++ icecreamratings/ratings/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 
 from flavors.models import Flavor
-from store.exceptions import OutOfStock
+from store.exceptions import OutOfStock, CorruptedDatabase
 
 
 def list_flavor_line_item(sku):
@@ -11,6 +11,9 @@ def list_flavor_line_item(sku):
     except Flavor.DoesNotExist:
         msg = "We are out of {0}".format(sku)
         raise OutOfStock(msg)
+    except Flavor.MultipleObjectsReturned:
+        msg = "Multiple items have SKU {}. Please fix!".format(sku)
+        raise CorruptedDatabase
 
 
 def list_any_line_item(model, sku):
--- icecreamratings/store/exceptions.py
+++ icecreamratings/store/exceptions.py
@@ -2,3 +2,7 @@
 
 class OutOfStock(Exception):
     pass
+
+
+class CorruptedDatabase(Exception):
+    pass
