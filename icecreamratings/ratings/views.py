60368d886404372ded1d22c60ac36a79daf2daf5
yurychu <tesewar@mail.ru>
2016-03-16 21:00:05 +0300

    использование исключений отсутствия объектов

--- icecreamratings/ratings/views.py
+++ icecreamratings/ratings/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
+from flavors.models import Flavor
+from store.exceptions import OutOfStock
+
+
+def list_flavor_line_item(sku):
+    try:
+        return Flavor.objects.get(sku=sku, quantity__gt=0)
+    except Flavor.DoesNotExist:
+        msg = "We are out of {0}".format(sku)
+        raise OutOfStock(msg)
+
+
+def list_any_line_item(model, sku):
+    try:
+        return model.objects.get(sku=sku, quantity__gt=0)
+    except ObjectDoesNotExist:
+        msg = "We are out of {0}.format(sku)"
+        raise OutOfStock(msg)
---
+++ icecreamratings/store/exceptions.py
@@ -0,0 +1,4 @@
+
+
+class OutOfStock(Exception):
+    pass
