76d84385a1f8b7737e82e6bbccfad3c61facfcf5
yurychu <tesewar@mail.ru>
2016-03-18 21:23:39 +0300

    use util func in CBV

--- icecreamratings/flavors/views.py
+++ icecreamratings/flavors/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
 
 from core.utils import check_sprinkle_rights
 from .models import Sprinkle
@@ -36,3 +37,16 @@ def sprinkle_preview(request):
     return render(request,
                   'sprinkle/sprinkle_preview.html',
                   {'sprinkle': sprinkle})
+
+
+class SprinkleDetail(DetailView):
+    """
+    Стандартный представление подробностей
+    """
+    model = Sprinkle
+
+    def dispatch(self, request, *args, **kwargs):
+        request = check_sprinkle_rights(request)
+        return super(SprinkleDetail, self).dispatch(
+            request, *args, **kwargs
+        )
