dfe339a4ff0d9cdd3d6a18b05f68b3acac4b4bd1
yurychu <tesewar@mail.ru>
2016-03-18 21:06:08 +0300

    use dynamic request

--- icecreamratings/core/utils.py
+++ icecreamratings/core/utils.py
@@ -3,6 +3,13 @@ from django.core.exceptions import PermissionDenied
 
 def check_sprinkle_rights(request):
     if request.user.can_sprinkle or request.user.is_staff:
+        # если еще добавим это значение после проверки,
+        # то оно облегчит логику в наших шаблонах
+        # например, вместо этого
+        # {% if request.user.can_sprinkle or request.user.is_staff %}
+        # можно просто выполнить
+        # {% if request.can_sprinkle %}
+        request.can_sprinkle = True
         return request
 
     # иначе возвращаем исключение 403
