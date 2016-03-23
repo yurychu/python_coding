6a58075b5f31c1eb40e6326dfd47e472ed2778cf
yurch <tesewar@mail.ru>
2016-03-23 17:25:16 +0300

    filtering needs data in views, not in template

--- icecreamratings/vouchers/views.py
+++ icecreamratings/vouchers/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import Voucher
+
+
+class GreenfeldRoyView(TemplateView):
+    template_name = 'vouchers/views_conditional.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(GreenfeldRoyView,
+                        self).get_context_data(**kwargs)
+        context['greenfelds'] = Voucher.objects.filter(
+            name__icontains='greenfeld')
+        context['roys'] = Voucher.objects.filter(
+            name__icontains='roy')
+        return context
+
