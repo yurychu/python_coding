17fa583003e1c5b26f76ae757f9fd0ddcb60a28b
yurychu <tesewar@mail.ru>
2016-03-24 23:01:23 +0300

    Environment object jinja2 and static load first template

--- icecreamratings/core/jinja2.py
+++ icecreamratings/core/jinja2.py
@@ -1,3 +1,5 @@
+import random
+
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.core.urlresolvers import reverse
 from django.template import defaultfilters
@@ -10,6 +12,10 @@ def environment(**options):
     env.globals.update({
         'static': staticfiles_storage.url,
         'url': reverse,
-        'dj': defaultfilters
+        'dj': defaultfilters,
+        # {{ random }}
+        'random_once': random.randint(1, 5),
+        # {{ random() }}
+        'random': lambda: random.randint(1, 5),
     })
     return env
